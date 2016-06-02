135e9b1d0197fca05489a455923c6e337fae7d7f
DanielitoAlbinsson <mr.daniel.albinsson@gmail.com>
2016-06-02 14:20:03 +0200

    wip stripe

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get 'search' => 'search#search'
   match '/users/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], :as => :finish_signup
+  resources :charges
 end
