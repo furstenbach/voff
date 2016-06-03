a842679989178c079bf1cd1ca8929933bda2863e
furstenbach <jenny.furstenbach@gmail.com>
2016-06-03 10:32:51 +0200

    Add columns to User, make rspec tests pass

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,16 @@
 require 'rails_helper'
+require '././app/models/user.rb'
 
 RSpec.describe User , type: :model do
-
+#describe User do
+      it { is_expected.to have_attribute :name }
+      it { is_expected.to have_attribute :email }
+      it { is_expected.to have_attribute :mobile }
+      it { is_expected.to have_attribute :address1 }
+      it { is_expected.to have_attribute :zip }
+      it { is_expected.to have_attribute :city }
+      it { is_expected.to have_attribute :latitude }
+      it { is_expected.to have_attribute :longitude }
+      it { is_expected.to have_attribute :presentation }
 
 end
